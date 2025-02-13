5c30a5e021c3a97e9881167822190d62c80a6f95
PetarKomatina00 <petarkkomatina00@gmail.com>
2025-02-13 08:18:57 +0100

    Initial commit

---
+++ target/debug/deps/WeatherApp-04474e9c8e34a0b9.d
@@ -0,0 +1,5 @@
+C:\Users\Petar\Desktop\WeatherApp\target\debug\deps\libWeatherApp-04474e9c8e34a0b9.rmeta: src/main.rs
+
+C:\Users\Petar\Desktop\WeatherApp\target\debug\deps\WeatherApp-04474e9c8e34a0b9.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/WeatherApp-d294c6d4ccd36254.d
@@ -0,0 +1,5 @@
+C:\Users\Petar\Desktop\WeatherApp\target\debug\deps\libWeatherApp-d294c6d4ccd36254.rmeta: src/main.rs
+
+C:\Users\Petar\Desktop\WeatherApp\target\debug\deps\WeatherApp-d294c6d4ccd36254.d: src/main.rs
+
+src/main.rs:
