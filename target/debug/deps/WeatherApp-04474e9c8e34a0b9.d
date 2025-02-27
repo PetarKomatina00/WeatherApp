5d52741b3ce67aa6ff11e00e8eafb7b2ad5108bf
PetarKomatina00 <petarkkomatina00@gmail.com>
2025-02-27 18:48:38 +0100

    Reorganized folde structure on Git

--- target/debug/deps/WeatherApp-04474e9c8e34a0b9.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Petar\Desktop\WeatherApp\target\debug\deps\libWeatherApp-04474e9c8e34a0b9.rmeta: src/main.rs
-
-C:\Users\Petar\Desktop\WeatherApp\target\debug\deps\WeatherApp-04474e9c8e34a0b9.d: src/main.rs
-
-src/main.rs:
