ef3c9b777d45e18fd9c70ef6e32a2d122c5aa5af
PetarKomatina00 <petarkkomatina00@gmail.com>
2025-06-11 09:15:40 +0200

    Refactor jwt utility functions
    In this code:
    - We get access token from the /callback function.
    - The access token contains a custom made role called "user" made from JS function in the Auth0 Action Dashboard.
    - No need for decoding and calling /oauth/token endpoint.
    - The logic behind validating a token still lives

--- backend/src/auth0/auth0.rs
+++ backend/src/auth0/auth0.rs
@@ -1,4 +1,6 @@
 
+use std::env;
+
 use rocket::{http::{Cookie, CookieJar, SameSite, Status}, response::Redirect, serde::json::Json};
 use rocket_oauth2::{OAuth2, TokenResponse};
 
@@ -8,27 +10,32 @@ pub struct Auth0;
 #[get("/login")]
 pub fn login(oauth2: OAuth2<Auth0>, jar: &CookieJar<'_>) -> Redirect{
     //println!("Login: {:?}", jar);
+    dotenv::dotenv().ok();
+    //("prompt", "login"),
+    let audience = env::var("AUDIENCE").expect("Could not get Audience");
     let ouath = oauth2
         .get_redirect_extras(
             jar,
             &["openid", "profile", "email"],
-            &[("prompt", "login")],         
+            &[("prompt", "login"), ("screen_hint", "signup"), ("audience", &audience)],     
             )
         .unwrap();
     //println!("{:?}", ouath);
     ouath
 }
 
-#[get("/callback")]
-pub fn callback(token: TokenResponse<Auth0>, jar: &CookieJar<'_>) -> Redirect {
+#[get("/callback?<code>&<state>")]
+pub fn callback(code: String, state: String, token: TokenResponse<Auth0>, jar: &CookieJar<'_>) -> Result<Redirect, String> {
     //println!("Callback: {:?}", jar);
+    println!("Code from callback: {}", code);
+    println!("Token from callback: {}", token.access_token());
     jar.add_private(
         Cookie::build(("access_token", token.access_token().to_owned()))
             .path("/")
               .same_site(SameSite::None)
               .build());
     //println!("Callback222: {:?}", jar);
-    Redirect::to("http://127.0.0.1:8001")
+    Ok(Redirect::to("http://127.0.0.1:8001"))
 }
 #[get("/api/token")]
 pub fn api_token(jar: &CookieJar<'_>) -> Result<Json<String>, Status>{
