47eb93d79a14366548ca055dd2c7c77095b7d966
Petar Komatina <petarkkomatina00@gmail.com>
2026-08-23 14:45:23 +0200

    Added MCP Client and Server.  Successfully retrieving tools and calling get_weather by city name

---
+++ mcp_weather/main.py
@@ -0,0 +1,25 @@
+
+## This code is written only for testing purposes
+## It shows a successfull connection has been made between MCP client, server and claude
+
+import asyncio
+from mcp_weather.mcp_client import MCPWeatherClient
+from mcp_weather.mcp_server import mcp_server
+
+async def main():
+    weather_mcp = MCPWeatherClient(mcp_server)
+
+    tools = await weather_mcp.get_claude_tools()
+
+    print("Cladue tools")
+    print(tools)
+
+    result = await weather_mcp.call_tool("get_weather", {
+        "city" : "Madrid"
+    })
+
+    print("Weather results")
+    print(result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
