b9f8ac261ed0c94fd1317b66adedc432ed5c72ee
Petar Komatina <petarkkomatina00@gmail.com>
2026-08-25 13:43:24 +0200

    mcp_weather(server) is now a separate container in docker

--- mcp_weather/main.py
+++ mcp_weather/main.py
@@ -2,24 +2,28 @@
 ## This code is written only for testing purposes
 ## It shows a successfull connection has been made between MCP client, server and claude
 
-import asyncio
-from mcp_weather.mcp_client import MCPWeatherClient
-from mcp_weather.mcp_server import mcp_server
+# This now does not work because MCPClient is in another folder: python_claude
+# This is used only for testing purposes in development.
 
-async def main():
-    weather_mcp = MCPWeatherClient(mcp_server)
 
-    tools = await weather_mcp.get_claude_tools()
+# import asyncio
+# from python_claude.src.mcp_client.mcp_client import MCPWeatherClient
+# from mcp_weather.mcp_server import mcp_server
 
-    print("Cladue tools")
-    print(tools)
+# async def main():
+#     weather_mcp = MCPWeatherClient(mcp_server)
 
-    result = await weather_mcp.call_tool("get_weather", {
-        "city" : "Madrid"
-    })
+#     tools = await weather_mcp.get_claude_tools()
 
-    print("Weather results")
-    print(result)
+#     print("Cladue tools")
+#     print(tools)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+#     result = await weather_mcp.call_tool("get_weather", {
+#         "city" : "Madrid"
+#     })
+
+#     print("Weather results")
+#     print(result)
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
--- mcp_weather/mcp_server.py
+++ mcp_weather/mcp_server.py
@@ -4,52 +4,63 @@ import httpx;
 from mcp.server import MCPServer
 from dotenv import load_dotenv
 
-load_dotenv()
 
-mcp_server = MCPServer("mcp-weather")
 
-OPENWEATHER_API_KEY=os.getenv("WEATHER_API_KEY", "Cannot get openweather api key")
+class MCPWeatherServer:
+    def __init__(self):
+        load_dotenv()
 
-if not OPENWEATHER_API_KEY:
-    raise RuntimeError("Weather api key not found")
+        self.open_weather_api_key = os.getenv("WEATHER_API_KEY", "Cannot get openweather api key")
 
-OPENWEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+        if not self.open_weather_api_key:
+            raise RuntimeError("Weather API Key not found")
 
-@mcp_server.tool()
-async def get_weather(city: str):
-    """
-    Get current weather information for a city.
+        self.open_weather_url = "http://api.openweathermap.org/data/2.5/weather"
 
-    Use this tool when current weather informations are needed,
-    for camping or other outdoor activities.
+        self.mcp_server = MCPServer("mcp-weather")
 
-    """
-    async with httpx.AsyncClient() as client:
-        response = await client.get(
-            OPENWEATHER_URL,
-            params={
-                "q":city,
-                "appid" : OPENWEATHER_API_KEY,
-                "units": "metric"
+        self._register_tools()
+
+        
+    def _register_tools(self):
+        self.mcp_server.add_tool(self.get_weather)
+
+
+    async def get_weather(self, city: str):
+        """
+        Get current weather information for a city.
+
+        Use this tool when current weather informations are needed,
+        for camping or other outdoor activities.
+
+        """
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                self.open_weather_url,
+                params={
+                    "q":city,
+                    "appid" : self.open_weather_api_key,
+                    "units": "metric"
+                }
+            )
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            return {
+                "city": data["name"],
+                "temperature": data["main"]["temp"],
+                "feels_like": data["main"]["feels_like"],
+                "humidity": data["main"]["humidity"],
+                "weather": data["weather"][0]["description"],
+                "wind_speed": data["wind"]["speed"],
             }
-        )
 
-        response.raise_for_status()
-
-        data = response.json()
-
-        return {
-            "city": data["name"],
-            "temperature": data["main"]["temp"],
-            "feels_like": data["main"]["feels_like"],
-            "humidity": data["main"]["humidity"],
-            "weather": data["weather"][0]["description"],
-            "wind_speed": data["wind"]["speed"],
-        }
+    def run (self, host: str = "127.0.0.1", port: int = 8003):
+        self.mcp_server.run(transport="streamable-http", host=host, port=port)
 
 if __name__ == "__main__":
-    mcp_server.run(
-        transport="streamable-http",
-        host="127.0.0.1",
-        port=8003
-    )
+    server = MCPWeatherServer()
+
+    server.run()
